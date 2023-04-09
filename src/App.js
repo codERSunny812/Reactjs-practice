0e9e4a81a76e09b24fd7b896e30854c87d2277a4
codersunny812 <sengersunny448@gmail.com>
2023-04-09 16:46:05 +0530

    main js file

--- src/App.js
+++ src/App.js
@@ -1,69 +1,139 @@
-import React, { Component, useState }  from 'react';
-import logo from './logo.svg';
-import './App.css';
-import students from './propsInFunctionalComponents'; 
+// import React, { Component, useState }  from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import students from './propsInFunctionalComponents'; 
 // import   ClassComponent  from './ClassComponent';
-function App(){
+// function App(){
 
-    // let name="sunny ";
-    // let newName="hey guys its me sunny here ";
-    // function apple(){
-    //   name="hey it's me sunny here ";
-    //   alert(name);
-    //   alert("function is called ");
-    // }
-    
-    // now lets use state to update the data 
+      // states in functional Component
+    //  state to update the data 
     // const [data,setData]=useState("sunny");
     // function updateData(){
     //   setData("suny vlogs ")
     // }
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{data} </h2>
-          <students Name={"sunny"} email="sengersunny44@gmail.com"/>
-          {/* similarly you can pass many value in this   */}
-          <button onClick={updateData}>click me </button>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
 
+
+
+  // get input box 
+
+  // const [data,setData]=useState(null);
+
+  // function getData(val){
+  //   console.log(val.target.value);
+  //   setData(val.target.value);
+
+  // } 
+
+  
+
+
+
+
+    
+  
+    // return (
+    //   <div className="App">
+    //     <div className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+          
+
+    //       <h2>{data} </h2>
+
+    //       {/* props in react and you can use many values in this  */}
+
+    //       <students Name={"sunny"} email="sengersunny44@gmail.com"/>
+
+          
+
+    //       <button onClick={updateData}>click me </button>
+
+    //     </div>
+        
+
+    //     {/* input in   react */}
+
+    //   <input type="text"  onChange={getData}/>
+
+
+//       </div>
+//     );
+//   }
 
 // export default App;
 
-// jsx 
 
-// import User from './User';
+// // baisc form in react js 
 
-// function Component
+// import "./App.css"
+// import React, { useState } from "react"
+
 // function App(){
 
+//   const [name,setName]=useState(" ");
+//   const [term, setTerms]=useState(false);
+//   const [option, setOption]=useState("");
+
+//   function getFormData(e){
+//     console.log(name,option,term);
+//     e.preventDefault();
+
+//   }
 //   return(
-//     <div className="App">
-//       JSX ?
-//       <User />
+//     <div className="App"> 
+//       <h1>handle forms in react </h1>
+//       <form action="" onSubmit={getFormData}>
+//         <input type="text"  placeholder="enter your name" onChange={(e)=>setName(e.target.value)} />  <br /> <br />
+//         <select name="" id="" onChange={(e)=>setOption(e.target.value)}>
+
+//           <option value="">Marvel</option>
+//           <option value="">DC </option>
+
+//         </select>
+//         <br /> <br />
+//         <input type="checkbox" name="" id="" onChange={() =>setTerms(e.target.checked)} /> <span>accept the term and condition </span>
+
+//         <button> submit</button>
+
+//       </form>
+
 //     </div>
-//   );
+//   )
 // }
 
-// how to make the class Component from a function Component
+// export default App;
 
-//  import React , {Component } from 'react'
-//  class App extends Component{
+// condition rendering in react 
+
+// import "./App.css";
+// import React from "react";
+// import profile from "./condtionRendring";
+
+// function App(){
+//   return(
+//     <div className="App">
+//       <h1>condition rendring </h1>
+//       <profile />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// basic form validation 
+
+import "./App.css";
+import React from "react";
+import BasicForm from "./BasicFormValidation";
 
 
-  // render(){
-    // return ( 
-      // write your code 
-    // );
-  // }
-//  }
-
+function App(){
+  // const data=true;
+  return(
+    <div className="App">
+      <h1>Basic form validation  </h1>
+      <BasicForm />
+    </div>
+  ); 
+}
 
 export default App;
