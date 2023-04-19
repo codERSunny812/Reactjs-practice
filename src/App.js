3664092b3697d24753a4b596421f557142dcfce1
codersunny812 <sengersunny448@gmail.com>
2023-04-20 01:16:05 +0530

    react course

--- src/App.js
+++ src/App.js
@@ -330,16 +330,186 @@
 
 // useEffect in react js 
 
-import React, { useEffect } from "react";
-import './App.css';
+// import React, { useEffect } from "react";
+// import './App.css';
 
-function App(){
-  useEffect(()=>{
-    console.warn("useeffect");
-  })
+// function App(){
+//   useEffect(()=>{
+//     console.warn("useeffect");
+//   })
+//   return(
+//     <div className="App">
+//       <h1>useeffedct in react js </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//  useEffect with specific state 
+
+// import React, { useEffect } from "react";
+// import './App.css';
+
+// function App(){ 
+//   const [data,setData]=useState(10);
+//   const [count,setCount]=useState(100);
+  
+//   useEffect(()=>{
+//     console.warn("useffect is called ");
+//   },[data])
+//   return(
+//     <div className="App">
+//       <h1>useeffect in react js </h1> 
+//       <h2>count : {count}</h2>
+//       <h2>data : {data}</h2>
+//       <button onClick={()=>{setCount(count+1)}}>update the count </button>
+//       <button onClick={()=>{setData(data+1)}}>update the data</button>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// Style in react js 
+
+// import './App.css';
+// import React from 'react';
+// import './Style.css';
+// import style from './custom.module.css';
+ 
+// function App(){
+//   return(
+//     <div className="App">
+//       <h1 className='primary'>style sheet type 1 in react </h1>
+
+//       <h2 style={{color:'red',backgroundColor:'black'}}>Style Sheet 2 in react js </h2>
+
+//       <h2 className={style.main}>Style sheet 3 in react js (css with module)</h2>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+// Bootstrap in react js 
+
+// import './App.css';
+// import React from 'react';
+// import { Button } from 'bootstrap';
+
+// function App(){
+// return(
+// <div className="App">
+//   <h2>Install Bootstrap</h2>
+//   <button onClick={()=>{alert("hello")}}>Click me </button>
+// </div>
+// );
+
+
+// }
+
+// export default App;
+
+
+
+// handle array with list 
+
+// import './App.css';
+// import React from 'react';
+
+
+// function App(){
+//   const students=["sunny " , "shivam ", "manas ", "varun "];
+ 
+//   const name=["sunny " , "shivam ", "manas ", "varun "];
+
+  // now we want to store the data of 3-4 student along with thier name , email and phone number 
+
+  // const detail=[
+  //   {name:"sunny", contact:1234, email:"sengersunny448@gmail.com"}
+  //   , 
+  //   { name:"shivam", contact:123456, email:"sethshivam,448@gmail.com"} , 
+  //   {name:"manas", contact:1234, email:"manasbrn448@gmail.com"}
+  // ]
+    
+  // name.map((data)=>{
+  //   console.warn("my name is :", data);
+  // })
+
+  // we can also itrate the array with the help of for loop
+
+//   for (let index = 0; index < name.length; index++) {
+//     console.log("my name is using the for loop  is : " , name[index]);
+    
+//   }
+// return(
+// <div className="App">
+//   <h2>handle array with list </h2>
+
+  {/* you can also itrate the array in the return using the map function  */}
+
+//   {
+//     students.map((items)=>{
+//       <h3>{items}</h3>
+//     })
+//   }
+// <h4>{detail.name}</h4> 
+// {
+//     detail.map((items)=>{
+//       <h3>{items.name}</h3>
+//     })
+//   }
+//   </div>
+// );
+
+
+// }
+
+// export default App;
+
+
+
+
+// list with the bootsrap and unique key 
+
+ import './App.css';
+ import React from 'react';
+ import {Table} from 'react-bootstrap'
+ function App(){
+
+  const user=[
+    {name:"sunny ", age:21,gender:"male"},
+    {name:"shivam ",age:22,gender:"male"},
+    {name:"manas",age:21,gender:"male"}
+  ]
   return(
     <div className="App">
-      <h1>useeffedct in react js </h1>
-    </div>
+      <h1>list with bootstrap</h1>
+      <Table >
+        <tr > 
+          <td>Name</td>
+          <td>age</td>
+          <td>gender</td>
+        </tr>
+        {
+          user.map((items,i)=>
+          <tr  key={i}>
+            <td>{items.name}</td>
+            <td>{items.age}</td>
+            <td>{items.gender}</td>
+          </tr>
+          )
+        }
+      </Table>
+          </div>
   );
-}
+ }
+
+ export default App;
