92245cc9e227db9792a8744eda65999ac253dda1
codersunny812 <sengersunny448@gmail.com>
2023-04-22 19:14:50 +0530

    new components

---
+++ src/child.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Child(){
+    return (
+      <>
+        <td>sunny</td>
+        <td>pandey</td>
+      </>  
+
+    //   because you can't use table inside the div
+    )
+}
+
+export default Child;
